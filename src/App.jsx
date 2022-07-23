d76298acc6f41b1b989866ff0789dc3faf7ff3bb
chuanlingShi <94197901+chuanlingShi@users.noreply.github.com>
2022-07-23 12:42:58 -0400

    implemented SignupForm component

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Layout, Typography } from "antd";
+import { useState } from "react";
+import LoginForm from "./components/LoginForm";
+import SignupForm from "./components/SingupForm";
+
+const { Title } = Typography;
+const { Header, Content } = Layout;
+
+function App() {
+  const [authed, setAuthed] = useState(false);
+  return (
+    <Layout style={{ height: "100vh" }}>
+      <Header>
+      <div className="header">
+          <Title
+            level={2}
+            style={{ color: "white", lineHeight: "inherit", marginBottom: 0 }}
+          >
+            Ling Food
+          </Title>
+          <div>{authed ? "MyCart Placeholder" : <SignupForm />}</div>
+        </div>
+      </Header>
+      <Content
+        style={{
+          padding: "50px",
+          maxHeight: "calc(100% - 64px)",
+          overflowY: "auto",
+        }}
+      >
+        {authed ? (
+          <div>content placeholder</div>
+        ) : (
+          <LoginForm onSuccess={() => setAuthed(true)} />
+        )}
+      </Content>
+    </Layout>
+  );
+}
+
+export default App;
